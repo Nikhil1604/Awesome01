eabcef6798dd2f64b2052fc2186a47aee1b5b803
nikthewanted <77376892+nikthewanted@users.noreply.github.com>
2023-12-23 12:25:36 +0530

    awesome01

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, useColorScheme } from 'react-native';
 
 export default function App() {
+  
+  const isDarkMode= useColorScheme === 'dark'
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      
+      <View style={styles.container}>
+        <Text style={styles.whiteText}>Hello World !</Text>
+        <Text style={styles.blueText}>Nikhil Wanjare</Text>
+        <Text style={isDarkMode?styles.blueText:styles.whiteText}>Here to </Text>
+        <Text style={styles.whiteText}>React Native app</Text>
+      </View>
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+const styles=StyleSheet.create({
+    container:{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    whiteText:{
+      color:'red'
+    },
+    darkText:{
+      color:'black'
+    },
+    blueText:{
+      color:'blue'
+    }
+
+})
+
